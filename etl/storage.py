dda029451e641145c4b3aba105bd0cd1783419af
Sambiz88 <samygrant04@gmail.com>
2026-01-13 17:09:37 -0500

    Added change_attribute_name util and changed epsDelta for surprise in json file

--- etl/storage.py
+++ etl/storage.py
@@ -8,3 +8,13 @@ def read_json():
 def write_json(data):
     with open('data.json', 'w') as file:
         json.dump(data, file, indent=4)
+
+def change_attribute_name(old_key, new_key):
+
+    data_list = read_json()
+
+    for obj in data_list:
+        if old_key in obj:
+            obj[new_key] = obj.pop(old_key)
+
+    write_json(data_list)
